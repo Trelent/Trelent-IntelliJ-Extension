99ed3ead09a0dc563dfb3bad65e95db57cecf638
GitHub Action <action@github.com>
2022-02-12 01:19:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/calumbird/trelentintellijextension/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/calumbird/trelentintellijextension/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.calumbird.trelentintellijextension.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.calumbird.trelentintellijextension.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/calumbird/trelentintellijextension/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.calumbird.trelentintellijextension.services
+
+import com.github.calumbird.trelentintellijextension.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
