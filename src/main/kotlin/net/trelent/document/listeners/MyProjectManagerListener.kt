c6b16c99ee516bbbf8e457a853e1c6eb1808857b
Calum Bird <calum@trelent.net>
2023-03-14 12:35:06 -0300

    v1.8.6

--- src/main/kotlin/net/trelent/document/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/net/trelent/document/listeners/MyProjectManagerListener.kt
@@ -17,14 +17,14 @@ internal class MyProjectManagerListener : ProjectManagerListener {
         project.service<MyProjectService>()
 
         // Check if there is a new version available
-        var currentVersion = "1.8.5";
-        var latestVersion = "1.8.5";
+        var currentVersion = "1.8.6"
+        var latestVersion = "1.8.6"
         try{
             currentVersion = PluginManagerCore.getPlugin(PluginId.getId("net.trelent.document"))!!.version
             latestVersion = getLatestVersion()!!
         }
         catch(e: Exception){
-            printlnError("Cannot get current version");
+            printlnError("Cannot get current version")
         }
 
 
@@ -34,7 +34,7 @@ internal class MyProjectManagerListener : ProjectManagerListener {
             if(currentVersion < latestVersion) {
                 val versionNotification = Notification(
                     "Trelent Warning Notification Group",
-                    "Trelent - New Version Available",
+                    "Trelent - new version available",
                     "You are currently using Trelent version $currentVersion. The latest available version is $latestVersion. Please update to the most recent version to get the latest improvements, features and more. To do so, navigate to Settings -> Plugins -> Trelent -> Update.",
                     NotificationType.WARNING
                 )
