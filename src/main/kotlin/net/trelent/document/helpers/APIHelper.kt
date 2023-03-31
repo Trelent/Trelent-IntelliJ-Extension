21ccbf98abc56d6bf12ba66cebc7f0e708854913
Wyatt Stagg <coolrooswag@gmail.com>
2023-03-31 13:11:31 -0300

    Update APIHelper.kt

--- src/main/kotlin/net/trelent/document/helpers/APIHelper.kt
+++ src/main/kotlin/net/trelent/document/helpers/APIHelper.kt
@@ -20,25 +20,25 @@ private const val PARSE_URL = "https://code-parsing-server.fly.dev/parse"
 private const val VERSION_CHECK_URL          = "https://code-parsing-server.fly.dev/"
 
 // Prod Api
-const val LOGIN_URL                          = "https://prod-api.trelent.net/auth/login?mode=login&port="
-const val LOGOUT_URL                         = "https://prod-api.trelent.net/auth/logout?port="
-const val SIGNUP_URL                         = "https://prod-api.trelent.net/auth/login?mode=signup&port="
-private const val GET_CHECKOUT_URL           = "https://prod-api.trelent.net/billing/checkout?billing_plan=1"
-private const val GET_PORTAL_URL             = "https://prod-api.trelent.net/billing/portal"
-private const val WRITE_DOCSTRING_URL        = "https://prod-api.trelent.net/docs/docstring"
-private var CHECKOUT_RETURN_URL              = "https://prod-api.trelent.net/redirect?redirect_url="
-private var PORTAL_RETURN_URL                = "https://prod-api.trelent.net/redirect?redirect_url="
+//const val LOGIN_URL                          = "https://prod-api.trelent.net/auth/login?mode=login&port="
+//const val LOGOUT_URL                         = "https://prod-api.trelent.net/auth/logout?port="
+//const val SIGNUP_URL                         = "https://prod-api.trelent.net/auth/login?mode=signup&port="
+//private const val GET_CHECKOUT_URL           = "https://prod-api.trelent.net/billing/checkout?billing_plan=1"
+//private const val GET_PORTAL_URL             = "https://prod-api.trelent.net/billing/portal"
+//private const val WRITE_DOCSTRING_URL        = "https://prod-api.trelent.net/docs/docstring"
+//private var CHECKOUT_RETURN_URL              = "https://prod-api.trelent.net/redirect?redirect_url="
+//private var PORTAL_RETURN_URL                = "https://prod-api.trelent.net/redirect?redirect_url="
 
 
 // Dev Api
-//const val LOGIN_URL                          = "https://dev-api.trelent.net/auth/login?mode=login&port="
-//const val LOGOUT_URL                         = "https://dev-api.trelent.net/auth/logout?port="
-//const val SIGNUP_URL                         = "https://dev-api.trelent.net/auth/login?mode=signup&port="
-//private const val GET_CHECKOUT_URL           = "https://dev-api.trelent.net/billing/checkout?billing_plan=1"
-//private const val GET_PORTAL_URL             = "https://dev-api.trelent.net/billing/portal"
-//private const val WRITE_DOCSTRING_URL        = "https://dev-api.trelent.net/docs/docstring"
-//private var CHECKOUT_RETURN_URL              = "https://dev-api.trelent.net/redirect?redirect_url="
-//private var PORTAL_RETURN_URL                = "https://dev-api.trelent.net/redirect?redirect_url="
+const val LOGIN_URL                          = "https://dev-api.trelent.net/auth/login?mode=login&port="
+const val LOGOUT_URL                         = "https://dev-api.trelent.net/auth/logout?port="
+const val SIGNUP_URL                         = "https://dev-api.trelent.net/auth/login?mode=signup&port="
+private const val GET_CHECKOUT_URL           = "https://dev-api.trelent.net/billing/checkout?billing_plan=1"
+private const val GET_PORTAL_URL             = "https://dev-api.trelent.net/billing/portal"
+private const val WRITE_DOCSTRING_URL        = "https://dev-api.trelent.net/docs/docstring"
+private var CHECKOUT_RETURN_URL              = "https://dev-api.trelent.net/redirect?redirect_url="
+private var PORTAL_RETURN_URL                = "https://dev-api.trelent.net/redirect?redirect_url="
 
 
 // Local Api
